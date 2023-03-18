d8c03a9dfa4b50a7dfafe679fd4b50bd698ef847
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-03-18 19:53:21 +0100

    Add filesystem mounts

---
+++ filesystem.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+{
+    fileSystems = {
+        "/" = {
+            device  = "/dev/disk/by-label/NIXOS";
+            fsType  = "btrfs";
+            options = [ "noatime" "compress=zstd" "autodefrag" "ssd" "space_cache" "subvol=root" ];
+        };
+        "/home" = {
+            device  = "/dev/disk/by-label/NIXOS";
+            fsType  = "btrfs";
+            options = [ "noatime" "compress=zstd" "autodefrag" "ssd" "space_cache" "subvol=home" ];
+        };
+        "/nix" = {
+            device  = "/dev/disk/by-label/NIXOS";
+            fsType  = "btrfs";
+            options = [ "noatime" "compress=zstd" "autodefrag" "ssd" "space_cache" "subvol=nix" ];
+        };
+        "/persist" = {
+            device  = "/dev/disk/by-label/NIXOS";
+            fsType  = "btrfs";
+            options = [ "noatime" "compress=zstd" "autodefrag" "ssd" "space_cache" "subvol=persist" ];
+        };
+        "/var/log" = {
+            device  = "/dev/disk/by-label/NIXOS";
+            fsType  = "btrfs";
+            options = [ "noatime" "compress=zstd" "autodefrag" "ssd" "space_cache" "subvol=log" ];
+            neededForBoot = true;
+        };
+        "/data" = {
+            device  = "/dev/disk/by-label/DATA";
+            fsType  = "btrfs";
+            options = [ "noatime" "compress=zstd" "autodefrag" "space_cache" "subvol=data" ];
+        };
+    };
+}
