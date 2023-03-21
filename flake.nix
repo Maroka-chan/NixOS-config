cd4f556bd869ada871c31bb01c3641d16a671bf0
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-03-21 22:19:57 +0100

    Add system flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "My NixOS configuration";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-small";
+    agenix.url = "github:ryantm/agenix";
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
+    arion.url = "github:hercules-ci/arion";
+    arion.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, nixpkgs, agenix, arion }: {
+    nixosConfigurations = {
+      my-nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+        # Add agenix and arion to the system
+        environment.systemPackages = with pkgs; [
+          agenix
+          arion
+        ];
+      };
+    };
+  };
+}
