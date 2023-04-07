cdf2af98a424314c773f6be649a0d9ee02fe7133
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-04-07 15:32:07 +0200

    Change secrets path

--- modify-secrets.sh
+++ modify-secrets.sh
@@ -2,8 +2,8 @@
 
 pushd ~/.dotfiles || exit
 
-SECRETS_DIR=.secrets
 SOPS_KEYS_DIR=/var/lib/sops
+SECRETS_DIR="$SOPS_KEYS_DIR"/.secrets
 SOPS_KEYS="$SOPS_KEYS_DIR"/keys.txt
 EDITOR=${EDITOR:-nvim}
 
@@ -31,7 +31,7 @@ EOL
 
 
 # Generate secrets directory if not present
-[ ! -d $SECRETS_DIR ] && mkdir $SECRETS_DIR
+[ ! -d $SECRETS_DIR ] && sudo mkdir $SECRETS_DIR
 
 
 sudo nix-shell -p sops --run "EDITOR=$EDITOR SOPS_AGE_KEY_FILE=$SOPS_KEYS sops ${SECRETS_DIR}/${HOSTNAME}.yaml"
