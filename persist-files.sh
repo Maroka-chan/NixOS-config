5418a1231c2defd4677ee3129129ccac1e9e349c
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-04-08 13:26:24 +0200

    Add script for moving files to persist

---
+++ persist-files.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+PERSIST_DIR=/persist
+
+sudo mkdir -p ${PERSIST_DIR}/etc/ssh
+sudo mkdir -p ${PERSIST_DIR}/var/lib
+
+# sudo cp {,/persist}/etc/nixos
+# sudo cp {,/persist}/var/lib/sops
+sudo cp {,$PERSIST_DIR}/etc/ssh/ssh_host_ed25519_key
+sudo cp {,$PERSIST_DIR}/etc/ssh/ssh_host_ed25519_key.pub
+sudo cp {,$PERSIST_DIR}/etc/NIXOS
+sudo cp {,$PERSIST_DIR}/etc/machine-id
