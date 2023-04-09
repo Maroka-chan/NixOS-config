93052e2eebf75562a72fec2626c459e4c3a30356
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-04-09 16:47:47 +0200

    Add public age key for encryption

--- scripts/nixos/create-secrets-file.sh
+++ scripts/nixos/create-secrets-file.sh
@@ -5,9 +5,10 @@ pushd "$(dirname -- "${BASH_SOURCE[0]}")"/../../ &>/dev/null || exit 1
 SECRETS_DIR=system/"$HOSTNAME"/.secrets
 SOPS_KEYS=/persist/var/lib/sops/keys.txt
 SECRETS_FILE=${SECRETS_DIR}/${HOSTNAME}.yaml
+PUB_KEY=$(sudo nix-shell -p age --run "age-keygen -y $SOPS_KEYS")
 
 [ ! -f "$SECRETS_FILE" ] &&
         echo "example: value" | sudo tee "$SECRETS_FILE" &>/dev/null &&
-        sudo nix-shell -p sops --run "SOPS_AGE_KEY_FILE=$SOPS_KEYS sops -e -i ${SECRETS_FILE}"
+        sudo nix-shell -p sops --run "sops --age $PUB_KEY -e -i $SECRETS_FILE"
 
 popd &>/dev/null || exit 1
