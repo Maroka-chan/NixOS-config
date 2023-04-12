5ecdfb66b0a9a5d22d72eb5806b207e67b9c8e68
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-04-12 12:40:04 +0200

    Move secrets outside git project

--- scripts/nixos/generate-age-keys.sh
+++ scripts/nixos/generate-age-keys.sh
@@ -2,16 +2,16 @@
 
 pushd "$(dirname -- "${BASH_SOURCE[0]}")"/../../ &>/dev/null || exit 1
 
-SOPS_KEYS_DIR=/persist/var/lib/sops
-SECRETS_DIR=system/"$HOSTNAME"/.secrets
-SOPS_KEYS="$SOPS_KEYS_DIR"/keys.txt
+SOPS_DIR=/persist/var/lib/sops
+SECRETS_DIR="$SOPS_DIR"/.secrets
+SOPS_KEYS="$SOPS_DIR"/keys.txt
 
 [ -d $SECRETS_DIR ] && sudo test ! -f $SOPS_KEYS &&
         echo "Secrets exist, but no key found for decryption." && exit
 
 # Generate age keys if not present
 if sudo test ! -f $SOPS_KEYS; then
-        sudo mkdir -p $SOPS_KEYS_DIR
+        sudo mkdir -p $SOPS_DIR
         sudo nix-shell -p age --run "age-keygen -o $SOPS_KEYS" ||
                 ( echo "Failed to generate age keys"; exit )
 fi
