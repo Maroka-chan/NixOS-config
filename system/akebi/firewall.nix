b7290caef7a5a785ffad5c85181193c0ea0395a0
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-04-13 00:58:40 +0200

    Add jellyfin as a confined systemd service

--- system/akebi/firewall.nix
+++ system/akebi/firewall.nix
@@ -4,9 +4,6 @@
     networking.firewall = {
         enable = true;
         allowPing = false;
-        allowedTCPPorts = [
-            22          # SSH
-            8096 8920   # Jellyfin
-        ];
+        allowedTCPPorts = [ ];
     };
 }
