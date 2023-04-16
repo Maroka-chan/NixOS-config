d7170b3b27af3f6f85b71986bcaf7d5a3c98e455
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-04-16 21:24:14 +0200

    Add deployment user
    The user is for non-interactive deployment.
    Therefore an extra sudo rule is also added.
    This allows for passwordless privilege escalation.

--- system/akebi/users.nix
+++ system/akebi/users.nix
@@ -1,12 +1,27 @@
 { config, pkgs, ... }:
+let
+    deployment_user = "deploy";
+in
 {
     users.mutableUsers = false;
 
-    users.users.maroka = {
-        isNormalUser = true;
+    users.users."${deployment_user}" = {
+        # isNormalUser = true;
         extraGroups = [ "wheel" ];
-        passwordFile = config.sops.secrets.maroka-password.path;
-        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLXkO6gEHyTSm+CJuhWPQRMJTM7psG2JzBROSTbK8op maroka@Arch-Desktop" ];
+        openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDe1OWBkEGP8kd4nJz280NaPLGVTyBj1IW6uvcLaNoDM2wM+i7hKKisYJCOdOxrkFxS0I0XoX88MPN0axel4jvmXNUHy/czi3TJTG6mLufxFF+UMcooJWHaExwfJSkYx+2/Ufk8bYbB0PJ3eLvpwRBywt4Qv9HrSLI1kFJfPSh+03IcIXz3+RJ7mz37j8Li2DTeWbRkZ9OJrUeT+ciKdnGK6p+2PotC2uWijhbXOENwy0Huu80WYIypOdwBcRQRAFE6G2OLX8rj7TqOj01tRSrrvjq9XUR+xlK6VCo2HY3b7NHve9j1kRgsHI2h99yiRWEJ1PXSU8aicIVY4qWgYowyYFT14XjD5aKeO5KHb6ULXcDHm4QteEuAr7vTROPM0nX/fF6UkeHJ9M1OyOGxIwIHsxpoERohH5sY4cXo4hKPn1P476YsLi765Smn2awCzFV89k0GFOYNX27gAniuMQTpPSSgTfFnNMzUVVdFRMwv82WvonkmkmwJ/+u6NAX/P0k=" ];
         packages = with pkgs; [];
     };
+
+    # Allow the deployment user to run any command as root without a password
+    security.sudo.extraRules = [
+        {
+            users = [ "${deployment_user}" ];
+            commands = [ 
+                {
+                    command = "ALL";
+                    options = [ "SETENV" "NOPASSWD" ];
+                }
+            ];
+        }
+    ];
 }
