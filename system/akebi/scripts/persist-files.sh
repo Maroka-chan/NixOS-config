7e024bf1e9863f4e366dfaacd4870bfe6fe5b185
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-05-10 23:47:21 +0200

    Update persist-files script

--- system/akebi/scripts/persist-files.sh
+++ system/akebi/scripts/persist-files.sh
@@ -4,6 +4,6 @@ PERSIST_DIR=/mnt/persist
 
 sudo mkdir -p "$PERSIST_DIR"/etc/ssh
 
-sudo cp {$1,"$PERSIST_DIR"}/etc/ssh/ssh_host_ed25519_key
-sudo cp {$1,"$PERSIST_DIR"}/etc/ssh/ssh_host_ed25519_key.pub
-sudo cp {$1,"$PERSIST_DIR"}/etc/machine-id
+sudo cp {,"$PERSIST_DIR"}/etc/ssh/ssh_host_ed25519_key
+sudo cp {,"$PERSIST_DIR"}/etc/ssh/ssh_host_ed25519_key.pub
+sudo cp {,"$PERSIST_DIR"}/etc/machine-id
