15fe9423a1ab1ed13162ce2e375a2df91c39d5a9
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-06-16 15:40:45 +0200

    Fix let statement

--- modules/btrfs-impermanence/default.nix
+++ modules/btrfs-impermanence/default.nix
@@ -40,31 +40,33 @@ in {
       umount /mnt
     '';
 
-    # Tools
-    fs-diff = pkgs.writeScriptBin "fs-diff" ''
-        set -euo pipefail
+    environment.systemPackages = 
+    let
+      # Tools
+      fs-diff = pkgs.writeScriptBin "fs-diff" ''
+          set -euo pipefail
 
-        OLD_TRANSID=$(sudo btrfs subvolume find-new /mnt/root-blank 9999999)
+          OLD_TRANSID=$(sudo btrfs subvolume find-new /mnt/root-blank 9999999)
 
-        sudo btrfs subvolume find-new "/mnt/root" "$OLD_TRANSID" |
-        sed '$d' |
-        cut -f17- -d' ' |
-        sort |
-        uniq |
-        while read path; do
-          path="/$path"
-          if [ -L "$path" ]; then
-            : # The path is a symbolic link, so is probably handled by NixOS already
-          elif [ -d "$path" ]; then
-            : # The path is a directory, ignore
-          else
-            echo "$path"
-          fi
-        done
-      '';
-
-    environment.systemPackages = with pkgs; [
+          sudo btrfs subvolume find-new "/mnt/root" "$OLD_TRANSID" |
+          sed '$d' |
+          cut -f17- -d' ' |
+          sort |
+          uniq |
+          while read path; do
+            path="/$path"
+            if [ -L "$path" ]; then
+              : # The path is a symbolic link, so is probably handled by NixOS already
+            elif [ -d "$path" ]; then
+              : # The path is a directory, ignore
+            else
+              echo "$path"
+            fi
+          done
+        '';
+    in
+      with pkgs; [
         fs-diff
-    ];
+      ];
   };
 }
