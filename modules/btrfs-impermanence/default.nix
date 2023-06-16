d34048b312a61310df60b976f4f332647224f096
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-06-16 17:21:58 +0200

    Update fs-diff script

--- modules/btrfs-impermanence/default.nix
+++ modules/btrfs-impermanence/default.nix
@@ -48,32 +48,8 @@ in {
     environment.systemPackages = 
     let
       # Tools
-      fs-diff = pkgs.writeScriptBin "fs-diff" ''
-          set -euo pipefail
-
-          mkdir -p /mnt
-          mount -t btrfs ${cfg.root} /mnt
-
-          OLD_TRANSID=$(sudo btrfs subvolume find-new /mnt/${cfg.blank-root-subvol} 9999999)
-
-          sudo btrfs subvolume find-new "/${cfg.root-subvol}" "$OLD_TRANSID" |
-          sed '$d' |
-          cut -f17- -d' ' |
-          sort |
-          uniq |
-          while read path; do
-            path="/$path"
-            if [ -L "$path" ]; then
-              : # The path is a symbolic link, so is probably handled by NixOS already
-            elif [ -d "$path" ]; then
-              : # The path is a directory, ignore
-            else
-              echo "$path"
-            fi
-          done
-
-          umount /mnt
-        '';
+      fs-diff-script = ./. + "/setup.sh";
+      fs-diff = pkgs.writeScriptBin "fs-diff" "${fs-diff-script}";
     in
       with pkgs; [
         fs-diff
