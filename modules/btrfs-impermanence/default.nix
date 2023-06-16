25d4cacac88fae58574c7cbdebe882ec0b8c5140
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-06-16 15:35:36 +0200

    Add fs-diff to btrfs-impermanence module
    Script to show the difference between the root and blank root subvolumes

--- modules/btrfs-impermanence/default.nix
+++ modules/btrfs-impermanence/default.nix
@@ -39,5 +39,32 @@ in {
 
       umount /mnt
     '';
+
+    # Tools
+    fs-diff = pkgs.writeScriptBin "fs-diff" ''
+        set -euo pipefail
+
+        OLD_TRANSID=$(sudo btrfs subvolume find-new /mnt/root-blank 9999999)
+
+        sudo btrfs subvolume find-new "/mnt/root" "$OLD_TRANSID" |
+        sed '$d' |
+        cut -f17- -d' ' |
+        sort |
+        uniq |
+        while read path; do
+          path="/$path"
+          if [ -L "$path" ]; then
+            : # The path is a symbolic link, so is probably handled by NixOS already
+          elif [ -d "$path" ]; then
+            : # The path is a directory, ignore
+          else
+            echo "$path"
+          fi
+        done
+      '';
+
+    environment.systemPackages = with pkgs; [
+        fs-diff
+    ];
   };
 }
