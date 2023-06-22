7348971a045eb01bb319a3121b5291e938645401
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-06-22 22:11:45 +0200

    Fix cursor such that the size is consistent

--- hosts/aisaka/home.nix
+++ hosts/aisaka/home.nix
@@ -38,11 +38,13 @@ in
     ];
   };
 
+  gtk.enable = true; # Needs to be enabled for home.pointerCursor to actually write gtk XDG settings files
   home.pointerCursor = {
     gtk.enable = true;
+    x11.enable = true;
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
-    size = 22;
+    size = 24;
   };
 
   # Hyprland
