9e9a50ba9040b8d762ade0e0419bdda39a631b10
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-10-01 03:58:32 +0200

    Add devenv cachix

--- hosts/aisaka/configuration.nix
+++ hosts/aisaka/configuration.nix
@@ -214,10 +214,12 @@ in
     substituters = [
       "https://hyprland.cachix.org"
       "https://anyrun.cachix.org"
+      "https://devenv.cachix.org"
     ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     ];
   };
 
