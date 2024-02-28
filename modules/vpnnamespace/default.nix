6d9f33c1983e42d379dee548909132f9e3b73c74
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-02-28 01:11:41 +0100

    improve vpnnamespace module
    This commit adds a systemd option, which makes the
    service use the VPN netns and prevents DNS leaks.
    A wg-quick config can now be given directly, and
    you can now also define multiple VPNs.

--- modules/vpnnamespace/default.nix
+++ modules/vpnnamespace/default.nix
@@ -2,169 +2,274 @@
 with lib;
 let
   cfg = config.services.vpnnamespace;
-in {
-  options.services.vpnnamespace = {
-    enable = mkEnableOption (lib.mdDoc "VPN Namespace") // {
-      description = lib.mdDoc ''
-        Whether to enable the VPN namespace.
 
-        To access the namespace a veth pair is used to
-        connect the vpn namespace and the default namespace
-        through a linux bridge. One end of the pair is
-        connected to the linux bridge on the default namespace.
-        The other end is connected to the vpn namespace.
+  namespaceToService = name: def: {
+    description = "${name} network interface";
+    bindsTo = [ "netns@${name}.service" ];
+    requires = [ "network-online.target" ];
+    after = [ "netns@${name}.service" ];
+    wantedBy = [ "multi-user.target" ];
 
-        Systemd services can be run within the namespace by
-        adding these options:
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
 
-        bindsTo = [ "netns@wg.service" ];
-        requires = [ "network-online.target" ];
-        after = [ "wg.service" ];
-        serviceConfig = {
-          NetworkNamespacePath = "/var/run/netns/wg";
-        };
-      '';
-    };
+      ExecStart = let vpnUp = pkgs.writeShellApplication {
+        name = "${name}-up";
 
-    accessibleFrom = mkOption {
-      type = types.listOf types.str;
-      default = [ ];
-      description = lib.mdDoc ''
-        Subnets or specific addresses that the namespace should be accessible to.
-      '';
-      example = [
-        "10.0.2.0/24"
-        "192.168.1.27"
-      ];
-    };
+        runtimeInputs = with pkgs; [ iproute2 wireguard-tools iptables ];
 
-    namespaceAddress = mkOption {
-      type = types.str;
-      default = "192.168.15.1";
-      description = lib.mdDoc ''
-        The address of the veth interface connected to the vpn namespace.
-        
-        This is the address used to reach the vpn namespace from other
-        namespaces connected to the linux bridge.
-      '';
-    };
+        text = ''
+          # Set up the wireguard interface
+          ip netns add ${name}
+          ip link add ${name}0 type wireguard
+          ip link set ${name}0 netns ${name}
 
-    bridgeAddress = mkOption {
-      type = types.str;
-      default = "192.168.15.5";
-      description = lib.mdDoc ''
-        The address of the linux bridge on the default namespace.
+          # Parse wireguard INI config file
+          # shellcheck disable=SC1090
+          source <(grep '=' ${def.wireguardConfigFile} | tr -d ' ')
 
-        The linux bridge sits on the default namespace and
-        needs an address to make communication between the
-        default namespace and other namespaces on the
-        bridge possible.
-      '';
-    };
+          # Add DNS
+          mkdir -p /etc/netns/${name}
+          echo "nameserver $DNS" > /etc/netns/${name}/resolv.conf
 
-    wireguardAddressPath = mkOption {
-      type = types.path;
-      default = "";
-      description = lib.mdDoc ''
-        The address for the wireguard interface.
-        It is a path to a file containing the address.
-        This is done so the whole wireguard config can be specified
-        in a secret file, such as a yaml file for sops-nix.
-      '';
-    };
+          # Add Addresses
+          IFS=","
+          # shellcheck disable=SC2154
+          for addr in $Address; do
+            if [[ "$addr" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/?[0-9]{1,2}$ ]]
+            then
+              ip -n ${name} address add "$addr" dev ${name}0
+            else
+              ip -6 -n ${name} address add "$addr" dev ${name}0
+            fi
+          done
 
-    wireguardConfigFile = mkOption {
-      type = types.path;
-      default = "/etc/wireguard/wg0.conf";
-      description = lib.mdDoc ''
-        Path to the wireguard config to use.
-        
-        Note that this is not a wg-quick config.
-      '';
-    };
+          # Set wireguard config
+          WGPVK=$(mktemp /tmp/wgpvk.XXXXXX)
+          chmod 400 "$WGPVK"
+          trap 'rm -f $WGPVK' EXIT
+          # shellcheck disable=SC2154
+          echo "$PrivateKey" > "$WGPVK"
 
-    portMappings = mkOption {
-      type = with types; listOf (attrsOf port);
-      default = [];
-      description = lib.mdDoc ''
-        A list of pairs mapping a port from the host to a port in the namespace.
-      '';
-      example = [{
-        From = 80;
-        To = 80;
-      }];
+          # shellcheck disable=SC2154
+          ip netns exec ${name} \
+            wg set ${name}0 \
+              private-key "$WGPVK" \
+              peer "$PublicKey" \
+              allowed-ips "$AllowedIPs" \
+              endpoint "$Endpoint"
+
+          rm -f "$WGPVK"
+
+          ip -n ${name} link set ${name}0 up
+          ip -n ${name} route add default dev ${name}0
+
+          # Start the loopback interface
+          ip -n ${name} link set dev lo up
+
+          # Create a bridge
+          ip link add ${name}-br type bridge
+          ip addr add ${def.bridgeAddress}/24 dev ${name}-br
+          ip link set dev ${name}-br up
+
+          # Set up veth pair to link namespace with host network
+          ip link add veth-${name}-br type veth peer name veth-${name} netns ${name}
+          ip link set veth-${name}-br master ${name}-br
+
+          ip -n ${name} addr add ${def.namespaceAddress}/24 dev veth-${name}
+          ip -n ${name} link set dev veth-${name} up
+        ''
+        # Add routes to make the namespace accessible
+        + strings.concatMapStrings (x: "ip -n ${name} route add ${x} via ${def.bridgeAddress}" + "\n") def.accessibleFrom
+        # Add prerouting rules
+        + strings.concatMapStrings (x: "iptables -t nat -A PREROUTING -p tcp --dport ${builtins.toString x.From} -j DNAT --to-destination ${def.namespaceAddress}:${builtins.toString x.To}" + "\n") def.portMappings;
+      }; in "${vpnUp}/bin/${name}-up";
+
+      ExecStopPost = let vpnDown = pkgs.writeShellApplication {
+        name = "${name}-down";
+
+        runtimeInputs = with pkgs; [ iproute2 iptables ];
+
+        text = ''
+          ip netns del ${name}
+          ip link del ${name}-br
+          ip link del veth-${name}-br
+          rm -rf /etc/netns/${name}
+        ''
+        # Delete prerouting rules
+        + strings.concatMapStrings (x: "iptables -t nat -D PREROUTING -p tcp --dport ${builtins.toString x.From} -j DNAT --to-destination ${def.namespaceAddress}:${builtins.toString x.To}" + "\n") def.portMappings;
+      }; in "${vpnDown}/bin/${name}-down";
     };
   };
 
-  config = mkIf cfg.enable {
+  vpnnamespaceOptions = { name, config, ... }: {
+    options = {
+      enable = mkEnableOption (lib.mdDoc "VPN Namespace") // {
+        description = lib.mdDoc ''
+          Whether to enable the VPN namespace.
+
+          To access the namespace a veth pair is used to
+          connect the vpn namespace and the default namespace
+          through a linux bridge. One end of the pair is
+          connected to the linux bridge on the default namespace.
+          The other end is connected to the vpn namespace.
+
+          Systemd services can be run within the namespace by
+          adding these options:
+
+          bindsTo = [ "netns@wg.service" ];
+          requires = [ "network-online.target" ];
+          after = [ "wg.service" ];
+          serviceConfig = {
+            NetworkNamespacePath = "/var/run/netns/wg";
+          };
+        '';
+      };
+
+      accessibleFrom = mkOption {
+        type = types.listOf types.str;
+        default = [ ];
+        description = lib.mdDoc ''
+          Subnets or specific addresses that the namespace should be accessible to.
+        '';
+        example = [
+          "10.0.2.0/24"
+          "192.168.1.27"
+        ];
+      };
+
+      namespaceAddress = mkOption {
+        type = types.str;
+        default = "192.168.15.1";
+        description = lib.mdDoc ''
+          The address of the veth interface connected to the vpn namespace.
+
+          This is the address used to reach the vpn namespace from other
+          namespaces connected to the linux bridge.
+        '';
+      };
+
+      bridgeAddress = mkOption {
+        type = types.str;
+        default = "192.168.15.5";
+        description = lib.mdDoc ''
+          The address of the linux bridge on the default namespace.
+
+          The linux bridge sits on the default namespace and
+          needs an address to make communication between the
+          default namespace and other namespaces on the
+          bridge possible.
+        '';
+      };
+
+      portMappings = mkOption {
+        type = with types; listOf (attrsOf port);
+        default = [];
+        description = lib.mdDoc ''
+          A list of pairs mapping a port from the host to a port in the namespace.
+        '';
+        example = [{
+          From = 80;
+          To = 80;
+        }];
+      };
+
+      wireguardConfigFile = mkOption {
+        type = types.path;
+        default = "/etc/wireguard/wg0.conf";
+        description = lib.mdDoc ''
+          Path to the wireguard config to use.
+
+          Note that this is not a wg-quick config.
+        '';
+      };
+    };
+
+    config = lib.mkIf config.enable {
+    };
+  };
+in {
+  options.systemd.services = lib.mkOption {
+    type = types.attrsOf (types.submodule ({ name, config, ... }: {
+      options.vpnconfinement = {
+        enable = lib.mkOption {
+          type = types.bool;
+          default = false;
+         # description = lib.mdDoc ''
+         # '';
+        };
+        vpnnamespace = lib.mkOption {
+          type = types.str;
+          default = "";
+        };
+      };
+
+      config = let
+        vpn = config.vpnconfinement.vpnnamespace;
+      in lib.mkIf config.vpnconfinement.enable {
+        bindsTo = [ "${vpn}.service" ];
+        after = [ "${vpn}.service" ];
+        wantedBy = [ "${vpn}.service" ];
+
+        serviceConfig = {
+          NetworkNamespacePath = "/var/run/netns/${vpn}";
+
+          BindReadOnlyPaths = [
+            "/etc/netns/${vpn}/resolv.conf:/etc/resolv.conf:norbind"
+            # "/etc/netns/${vpn}/nsswitch.conf:/etc/nsswitch.conf:norbind"
+            "/var/empty:/var/run/nscd:norbind"
+            "/var/empty:/var/run/resolvconf:norbind"
+          ];
+
+          #RootDirectory = "/var/empty";
+          #TemporaryFileSystem = "/";
+          PrivateMounts = lib.mkDefault true;
+          #ExecStartPre = "${pkgs.mount}/bin/mount --bind /var/empty /var/run/nscd";
+        };
+      };
+    }));
+  };
+
+  options.services.vpnnamespace = {
+    namespace = mkOption {
+      type = with types; attrsOf (submodule [ vpnnamespaceOptions ]);
+    };
+  };
+
+  config = {
     boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
-    systemd.services."netns@" = {
-      description = "%I network namespace";
-      before = [ "network.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = "${pkgs.iproute2}/bin/ip netns add %I";
-        ExecStop = "${pkgs.iproute2}/bin/ip netns del %I";
+   # environment.etc = {
+   #   "netns/wg/resolv.conf".text = ''
+   #     nameserver 100.64.0.23
+   #   '';
+   #   "netns/wg/nsswitch.conf".text = ''
+   #     passwd:    files systemd
+   #     group:     files [success=merge] systemd
+   #     shadow:    files
+
+   #     hosts:     dns
+   #     networks:  files
+
+   #     ethers:    files
+   #     services:  files
+   #     protocols: files
+   #     rpc:       files
+   #   '';
+   # };
+
+    systemd.services = {
+      "netns@" = {
+        description = "%I network namespace";
+        before = [ "network.target" ];
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+          ExecStart = "${pkgs.iproute2}/bin/ip netns add %I";
+          ExecStop = "${pkgs.iproute2}/bin/ip netns del %I";
+        };
       };
-    };
-
-    systemd.services.wg = {
-      description = "wg network interface";
-      bindsTo = [ "netns@wg.service" ];
-      requires = [ "network-online.target" ];
-      after = [ "netns@wg.service" ];
-      wantedBy = [ "multi-user.target" ];
-
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = with pkgs; writers.writeBash "wg-up" (''
-          set -e
-
-          # Set up the wireguard interface
-          ${iproute2}/bin/ip link add wg0 type wireguard
-          ${iproute2}/bin/ip link set wg0 netns wg
-          ${iproute2}/bin/ip -n wg address add $(cat ${cfg.wireguardAddressPath}) dev wg0
-          ${iproute2}/bin/ip netns exec wg \
-            ${wireguard-tools}/bin/wg setconf wg0 ${cfg.wireguardConfigFile}
-          ${iproute2}/bin/ip -n wg link set wg0 up
-          ${iproute2}/bin/ip -n wg route add default dev wg0
-
-          # Start the loopback interface
-          ${iproute2}/bin/ip -n wg link set dev lo up
-
-          # Create a bridge
-          ${iproute2}/bin/ip link add v-net-0 type bridge
-          ${iproute2}/bin/ip addr add ${cfg.bridgeAddress}/24 dev v-net-0
-          ${iproute2}/bin/ip link set dev v-net-0 up
-
-          # Set up veth pair to link namespace with host network
-          ${iproute2}/bin/ip link add veth-vpn-br type veth peer name veth-vpn netns wg
-          ${iproute2}/bin/ip link set veth-vpn-br master v-net-0
-
-          ${iproute2}/bin/ip -n wg addr add ${cfg.namespaceAddress}/24 dev veth-vpn
-          ${iproute2}/bin/ip -n wg link set dev veth-vpn up
-        ''
-
-        # Add routes to make the namespace accessible
-        + strings.concatMapStrings (x: "${iproute2}/bin/ip -n wg route add ${x} via ${cfg.bridgeAddress}" + "\n") cfg.accessibleFrom
-
-        # Add prerouting rules
-        + strings.concatMapStrings (x: "${iptables}/bin/iptables -t nat -A PREROUTING -p tcp --dport ${builtins.toString x.From} -j DNAT --to-destination ${cfg.namespaceAddress}:${builtins.toString x.To}" + "\n") cfg.portMappings);
-
-
-        ExecStopPost = with pkgs; writers.writeBash "wg-down" (''
-          ${iproute2}/bin/ip -n wg route del default dev wg0
-          ${iproute2}/bin/ip -n wg link del wg0
-          ${iproute2}/bin/ip -n wg link del veth-vpn
-          ${iproute2}/bin/ip link del v-net-0
-        ''
-
-        # Delete prerouting rules
-        + strings.concatMapStrings (x: "${iptables}/bin/iptables -t nat -D PREROUTING -p tcp --dport ${builtins.toString x.From} -j DNAT --to-destination ${cfg.namespaceAddress}:${builtins.toString x.To}" + "\n") cfg.portMappings);
-      };
-    };
+    } // mapAttrs' (n: v: nameValuePair n (namespaceToService n v)) cfg.namespace;
   };
 }
