a9aa7f9491643c4af266a67fd538ecd2d02ef577
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-03-22 15:48:07 +0100

    [v00334] add cachix substituter

--- hosts/v00334/configuration.nix
+++ hosts/v00334/configuration.nix
@@ -170,11 +170,13 @@
     auto-optimise-store = true;
     builders-use-substitutes = true;
     substituters = [
+      "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       "https://anyrun.cachix.org"
       "https://devenv.cachix.org"
     ];
     trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
