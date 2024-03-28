0fb6657382cc6ba570309d30ec71df746ff76bd2
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-03-28 22:59:29 +0100

    [kanan] replace anyrun w/ rofi

--- hosts/kanan/home.nix
+++ hosts/kanan/home.nix
@@ -31,8 +31,6 @@ in
     sshfs     # Remote filesystems over SSH
 
     webcord   # Third-party Discord
-
-    #deploy-rs # Deployment Tool
   ];
 
   programs = {
@@ -40,28 +38,209 @@ in
       enable = true;
     };
     customNeovim.enable = true;
-    anyrun = {
+    rofi = {
       enable = true;
-      config = {
-        plugins = [ inputs.anyrun.packages.${pkgs.system}.applications ];
-        width = { fraction = 0.3; };
-        hideIcons = false;
-        ignoreExclusiveZones = false;
-        layer = "overlay";
-        hidePluginInfo = false;
-        closeOnClick = true;
-        showResultsImmediately = false;
-        maxEntries = null;
+      package = pkgs.rofi-wayland;
+      extraConfig = {
+        modi = "drun,run,filebrowser,window";
+        show-icons = true;
+        display-drun = "APPS";
+        display-run = "RUN";
+        display-filebrowser = "FILES";
+        display-window = "WINDOW";
+        drun-display-format = "{name}";
+        window-format = "{w} · {c} · {t}";
       };
-      extraCss = ''
-        window {
-          background: transparent;
-        }
-      '';
+      theme = let
+        inherit (config.lib.formats.rasi) mkLiteral;
+      in {
+        "*" = {
+          font = "JetBrains Mono Nerd Font 10";
+          background = mkLiteral "#180F39";
+          background-alt = mkLiteral "#32197D";
+          foreground = mkLiteral "#FFFFFF";
+          selected = mkLiteral "#FF00F1";
+          active = mkLiteral "#9878FF";
+          urgent = mkLiteral "#7D0075";
+        };
+
+        window = {
+          transparency = "real";
+          location = mkLiteral "center";
+          anchor = mkLiteral "center";
+          fullscreen = false;
+          width = mkLiteral "1000px";
+          x-offset = mkLiteral "0px";
+          y-offset = mkLiteral "0px";
+          enabled = true;
+          border-radius = mkLiteral "15px";
+          cursor = "default";
+          background-color = mkLiteral "@background";
+        };
+
+        mainbox = {
+          enabled = true;
+          spacing = mkLiteral "0px";
+          background-color = mkLiteral "transparent";
+          orientation = mkLiteral "horizontal";
+          children = [ "imagebox" "listbox" ];
+        };
+
+        imagebox = {
+          padding = mkLiteral "20px";
+          background-color = mkLiteral "transparent";
+          background-image = mkLiteral ("url(" + "\"${dotfiles}/images/b.png\"" + ", height)");
+          orientation = mkLiteral "vertical";
+          children = [ "inputbar" "dummy" "mode-switcher" ];
+        };
+
+        listbox = {
+          spacing = mkLiteral "20px";
+          padding = mkLiteral "20px";
+          background-color = mkLiteral "transparent";
+          orientation = mkLiteral "vertical";
+          children = [ "message" "listview" ];
+        };
+
+        dummy = {
+          background-color = mkLiteral "transparent";
+        };
+
+        inputbar = {
+          enabled = true;
+          spacing = mkLiteral "10px";
+          padding = mkLiteral "15px";
+          border-radius = mkLiteral "10px";
+          background-color = mkLiteral "@background-alt";
+          text-color = mkLiteral "@foreground";
+          children = [ "textbox-prompt-colon" "entry" ];
+        };
+
+        textbox-prompt-colon = {
+          enabled = true;
+          expand = false;
+          str = "";
+          background-color = mkLiteral "inherit";
+          text-color = mkLiteral "inherit";
+        };
+
+        entry = {
+          enabled = true;
+          background-color = mkLiteral "inherit";
+          text-color = mkLiteral "inherit";
+          cursor = mkLiteral "text";
+          placeholder = "Search";
+          placeholder-color = mkLiteral "inherit";
+        };
+
+        mode-switcher = {
+          enabled = true;
+          spacing = mkLiteral "20px";
+          background-color = mkLiteral "transparent";
+          text-color = mkLiteral "@foreground";
+        };
+
+        button = {
+          padding = mkLiteral "15px";
+          border-radius = mkLiteral "10px";
+          background-color = mkLiteral "@background-alt";
+          text-color = mkLiteral "inherit";
+          cursor = mkLiteral "pointer";
+        };
+
+        "button selected" = {
+          background-color = mkLiteral "@selected";
+          text-color = mkLiteral "@foreground";
+        };
+
+        listview = {
+          enabled = true;
+          columns = 1;
+          lines = 8;
+          cycle = true;
+          dynamic = true;
+          scrollbar = false;
+          layout = mkLiteral "vertical";
+          reverse = false;
+          fixed-height = true;
+          fixed-columns = true;
+          spacing = mkLiteral "10px";
+          background-color = mkLiteral "transparent";
+          text-color = mkLiteral "@foreground";
+          cursor = "default";
+        };
+
+        element = {
+          enabled = true;
+          spacing = mkLiteral "15px";
+          padding = mkLiteral "8px";
+          border-radius = mkLiteral "10px";
+          background-color = mkLiteral "transparent";
+          text-color = mkLiteral "@foreground";
+          cursor = mkLiteral "pointer";
+        };
+        "element normal.normal" = {
+          background-color = mkLiteral "inherit";
+          text-color = mkLiteral "inherit";
+        };
+        "element normal.urgent" = {
+          background-color = mkLiteral "@urgent";
+          text-color = mkLiteral "@foreground";
+        };
+        "element normal.active" = {
+          background-color = mkLiteral "@active";
+          text-color = mkLiteral "@foreground";
+        };
+        "element selected.normal" = {
+          background-color = mkLiteral "@selected";
+          text-color = mkLiteral "@foreground";
+        };
+        "element selected.urgent" = {
+          background-color = mkLiteral "@urgent";
+          text-color = mkLiteral "@foreground";
+        };
+        "element selected.active" = {
+          background-color = mkLiteral "@urgent";
+          text-color = mkLiteral "@foreground";
+        };
+        element-icon = {
+          background-color = mkLiteral "transparent";
+          text-color = mkLiteral "inherit";
+          size = mkLiteral "32px";
+          cursor = mkLiteral "inherit";
+        };
+        element-text = {
+          background-color = mkLiteral "transparent";
+          text-color = mkLiteral "inherit";
+          cursor = mkLiteral "inherit";
+          vertical-align = mkLiteral "0.5";
+          horizontal-align = mkLiteral "0.0";
+        };
+
+        message = {
+          background-color = mkLiteral "transparent";
+        };
+
+        textbox = {
+          padding = mkLiteral "15px";
+          border-radius = mkLiteral "10px";
+          background-color = mkLiteral "@background-alt";
+          text-color = mkLiteral "@foreground";
+          vertical-align = mkLiteral "0.5";
+          horizontal-align = mkLiteral "0.0";
+        };
+
+        error-message = {
+          padding = mkLiteral "15px";
+          border-radius = mkLiteral "20px";
+          background-color = mkLiteral "@background";
+          text-color = mkLiteral "@foreground";
+        };
+      };
     };
     zsh = {
       enable = true;
-      enableAutosuggestions = true;
+      autosuggestion.enable = true;
       enableCompletion = true;
       syntaxHighlighting.enable = true;
 
@@ -244,7 +423,7 @@ in
       bind = $mainMod SHIFT, Q, killactive
       bind = $mainMod, F, fullscreen
       bind = $mainMod, M, fullscreen, 1
-      bind = $mainMod, D, exec, anyrun
+      bind = $mainMod, D, exec, rofi -show drun
       bind = $mainMod, Return, exec, alacritty
       bind = $mainMod, V, togglefloating
       bind = $mainMod, B, exec, librewolf
