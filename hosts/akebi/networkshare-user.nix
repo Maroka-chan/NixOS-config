c0447c029a30fac7ba3d314cb7bbb49fb0a93a51
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-04-07 23:41:21 +0200

    add kanan pubkey to akebi sshfs user

--- hosts/akebi/networkshare-user.nix
+++ hosts/akebi/networkshare-user.nix
@@ -6,6 +6,7 @@
         extraGroups = [ ];
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLXkO6gEHyTSm+CJuhWPQRMJTM7psG2JzBROSTbK8op maroka@Arch-Desktop"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyMZKyptGPtS/osbdmDrhnn2J08Iiy/i+BrvqvyNBpJ kanan"
         ];
         packages = with pkgs; [];
     };
