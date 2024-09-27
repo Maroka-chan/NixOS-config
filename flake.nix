94a20f073e00228040759001e8100f4463630fe7
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-09-27 21:44:04 +0200

    clean up inputs and flake

--- flake.nix
+++ flake.nix
@@ -7,13 +7,11 @@
     nixpkgs-small.url = "github:NixOS/nixpkgs/nixos-24.05-small";
     nixpkgs-master.url = "github:NixOS/nixpkgs";
     nixpkgs-stremio-server.url = "github:NixOS/nixpkgs/aae94e56a7b905281f007a5b70aa7ffff89aee57";
+
     impermanence.url = "github:nix-community/impermanence";
     agenix.url = "github:ryantm/agenix";
-    nixos-generators.url = "github:nix-community/nixos-generators";
-    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    deploy-rs.url = "github:serokell/deploy-rs";
 
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     hyprland.inputs.nixpkgs.follows = "nixpkgs-unstable";
@@ -21,21 +19,21 @@
       url = "github:Duckonaut/split-monitor-workspaces";
       inputs.hyprland.follows = "hyprland";
     };
-
-    hyprlock.url = "github:hyprwm/hyprlock";
-    hyprlock.inputs.nixpkgs.follows = "nixpkgs";
-    shutoku.url = "git+ssh://git@github.com/Maroka-chan/Shutoku-rs";
-    shutoku.inputs.nixpkgs.follows = "nixpkgs-unstable";
-    vpn-confinement.url = "github:Maroka-chan/VPN-Confinement";
-    vpn-confinement.inputs.nixpkgs.follows = "nixpkgs-unstable";
-    neovim.url = "github:Maroka-chan/nvim-config";
-    neovim.inputs.nixpkgs.follows = "nixpkgs-unstable";
-    tlock.url = "git+https://github.com/eklairs/tlock?submodules=1";
-    tlock.inputs.nixpkgs.follows = "nixpkgs-unstable";
     ags.url = "github:Aylur/ags";
 
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
+    deploy-rs.url = "github:serokell/deploy-rs";
+
+    tlock.url = "git+https://github.com/eklairs/tlock?submodules=1";
+    tlock.inputs.nixpkgs.follows = "nixpkgs-unstable";
+
+    neovim.url = "github:Maroka-chan/nvim-config";
+    neovim.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    shutoku.url = "git+ssh://git@github.com/Maroka-chan/Shutoku-rs";
+    shutoku.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    vpn-confinement.url = "github:Maroka-chan/VPN-Confinement";
+    vpn-confinement.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     umu = {
       url = "git+https://github.com/Open-Wine-Components/umu-launcher/?dir=packaging\/nix&submodules=1";
@@ -44,6 +42,7 @@
 
     hoyonix.url = "path:///home/maroka/Documents/hoyonix";
     hoyonix.inputs.nixpkgs.follows = "nixpkgs-unstable";
+
   };
 
   outputs = inputs @ { self, nixos-generators, deploy-rs, ... }:
@@ -52,16 +51,14 @@
 
     deploy.nodes.akebi = {
       hostname = "akebi";
-      profiles = {
-        system = {
-          sshUser = "deploy";
-          path =
-            deploy-rs.lib.x86_64-linux.activate.nixos
-            self.nixosConfigurations.akebi;
-          user = "root";
-        };
-      };
       remoteBuild = false;
+      profiles.system = {
+        user = "root";
+        sshUser = "deploy";
+        path =
+          deploy-rs.lib.x86_64-linux.activate.nixos
+          self.nixosConfigurations.akebi;
+      };
     };
 
     # This is highly advised, and will prevent many possible mistakes
@@ -69,16 +66,6 @@
       builtins.mapAttrs
         (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
 
-    packages = let
-      system = "x86_64-linux";
-    in {
-      "${system}" = {
-        installer-iso = nixos-generators.nixosGenerate {
-          inherit system;
-          format = "install-iso";
-          modules = [ ./iso.nix ];
-        };
-      };
     };
   };
 }
