bd055d171fa2cf1ade54ba4cef985aea19a6db19
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-10-26 22:19:43 +0200

    add sshfs user to media group

--- hosts/akebi/networkshare-user.nix
+++ hosts/akebi/networkshare-user.nix
@@ -3,7 +3,7 @@
 {
     users.users.sshfs = {
         isNormalUser = true;
-        extraGroups = [ ];
+        extraGroups = [ "media" ];
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLXkO6gEHyTSm+CJuhWPQRMJTM7psG2JzBROSTbK8op maroka@Arch-Desktop"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyMZKyptGPtS/osbdmDrhnn2J08Iiy/i+BrvqvyNBpJ kanan"
