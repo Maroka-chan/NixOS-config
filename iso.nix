22bc75b481312504ecaea64cbeab70965fca9f59
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-11-25 00:17:29 +0100

    cleanup unused scripts

--- iso.nix
+++
@@ -1,6 +0,0 @@
-{ ... }:
-{
-  isoImage.squashfsCompression = "gzip -Xcompression-level 1";
-  systemd.tmpfiles.rules = [ "d /dev/disk/by-partlabel 0755 root root" ];
-  users.users."nixos".openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyMZKyptGPtS/osbdmDrhnn2J08Iiy/i+BrvqvyNBpJ kanan" ];
-}
