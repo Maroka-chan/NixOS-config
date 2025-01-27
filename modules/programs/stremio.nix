9ae90fe7d01b6a4d67f97264c512dff089c1553e
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2025-01-28 00:01:51 +0100

    add impermanence section for stremio

--- modules/programs/stremio.nix
+++ modules/programs/stremio.nix
@@ -1,14 +1,22 @@
-{ pkgs, lib, config, ... }:
-with lib;
+{ pkgs, lib, config, username, ... }:
 let
   module_name = "stremio";
   cfg = config.configured.programs."${module_name}";
+  inherit (lib) mkMerge mkIf mkEnableOption;
 in {
   options.configured.programs."${module_name}" = {
     enable = mkEnableOption "Enable Stremio";
   };
 
-  config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ stremio ];
-  };
+  config = mkMerge [
+    (mkIf cfg.enable {
+      environment.systemPackages = with pkgs; [ stremio ];
+    })
+    (mkIf config.impermanence.enable {
+      home-manager.users.${username}.home.persistence
+      ."/persist/home/${username}".files = [
+        ".local/share/Smart Code ltd/Stremio"
+      ];
+    })
+  ];
 }
