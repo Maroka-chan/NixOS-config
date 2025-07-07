beac81635e7f2255f834b958030941f117436b1d
AlexBMJ <33891167+AlexBMJ@users.noreply.github.com>
2025-07-07 18:14:51 +0200

    [BMJ-Desktop] add mikuboot

--- hosts/BMJ-Desktop/hardware-configuration.nix
+++ hosts/BMJ-Desktop/hardware-configuration.nix
@@ -1,9 +1,16 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 {
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
+
+  boot.plymouth = {
+    enable = true;
+    themePackages = [ pkgs.mikuboot ];
+    theme = "mikuboot";
+  };
+
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
