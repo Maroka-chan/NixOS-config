c2002f5e948baac5f770b4096e480465e7c85298
AlexBMJ <33891167+AlexBMJ@users.noreply.github.com>
2025-10-06 11:37:56 +0200

    V00165: add new veo wallpaper

--- hosts/V00165/hyprland.nix
+++ hosts/V00165/hyprland.nix
@@ -14,7 +14,7 @@
       mode = preferred
       position = 1920x560
     }
-    
+
     monitorv2 {
       output = desc:Lenovo Group Limited P27h-20 V90A9AL0
       mode = preferred
@@ -22,7 +22,7 @@
       position = 4480x0
     }
 
-    exec-once = swaybg -i ${../../dotfiles/wallpapers/island-coast.jpg} -m fill
+    exec-once = swaybg -i ${../../dotfiles/wallpapers/Veo_Wallpaper1.jpg} -m fill
     exec-once = ags run --gtk 3
   '';
 }
