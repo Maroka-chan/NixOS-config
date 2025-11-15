a542014f3c07fb90f8adc0827a1899b3683bd458
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2025-11-15 22:15:10 +0100

    [aisaka] remove webcord

--- hosts/aisaka/home.nix
+++ hosts/aisaka/home.nix
@@ -4,7 +4,6 @@
     swaybg # Wallpaper Tool
 
     sshfs # Remote filesystems over SSH
-    webcord # Third-party Discord
   ];
 
   programs = {
@@ -26,14 +25,12 @@
       ".p10k.zsh"
       ".cache/gitstatus/gitstatusd-linux-x86_64"
       ".config/Mullvad VPN/gui_settings.json"
-      ".config/WebCord/config.json"
       ".config/btop/btop.conf"
       ".config/cat_installer/ca.pem" # eduroam wifi certificate
       ".local/share/nvim/telescope_history"
     ];
     directories = [
       ".local/share/Jellyfin Media Player/QtWebEngine/Default/Local Storage/leveldb"
-      ".config/WebCord/Local Storage/leveldb"
       ".local/share/direnv/allow"
       ".local/state/nvim/swap"
       ".local/state/nvim/shada"
