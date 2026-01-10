0fbdcb0a537ed66ff1f8a0ca939dbc60eca05e8e
AlexBMJ <33891167+AlexBMJ@users.noreply.github.com>
2026-01-11 00:48:56 +0100

    [BMJ-Duo] persist important directories

--- hosts/BMJ-Duo/home.nix
+++ hosts/BMJ-Duo/home.nix
@@ -47,4 +47,32 @@ in {
     };
     obs-studio.enable = true;
   };
+
+  # Home Manager Persistence
+  home.persistence."/persist" = {
+    files = [
+      ".cache/gitstatus/gitstatusd-linux-x86_64"
+      ".config/btop/btop.conf"
+      ".local/share/nvim/telescope_history"
+      ".config/vesktop/settings/settings.json"
+      ".config/vesktop/settings.json"
+      ".config/vesktop/state.json"
+      ".flutter"
+    ];
+    directories = [
+      "NixOS-config"
+      "Projects"
+      ".local/share/Jellyfin Media Player/QtWebEngine/Default/Local Storage/leveldb"
+      ".config/vesktop/sessionData/Local Storage/leveldb"
+      ".local/share/direnv/allow"
+      ".local/state/nvim/swap"
+      ".local/state/nvim/shada"
+      ".local/share/osu"
+      ".local/share/DaVinciResolve"
+      ".local/share/PrismLauncher"
+      ".dart-tool"
+      ".pub-cache"
+      ".local/share/Steam"
+    ];
+  };
 }
