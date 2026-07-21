08d651f6174c8dae1e50ab512ec0d22156feb7c8
Alexander Jacobsen <dev@alexbmj.com>
2026-07-21 21:33:45 +0200

    [BMJ-Duo] fix sleep issue by switching to stable kernel

--- hosts/BMJ-Duo/hardware-configuration.nix
+++ hosts/BMJ-Duo/hardware-configuration.nix
@@ -1,9 +1,14 @@
-{ config, lib, pkgs, ... }:
 {
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" "sdhci_pci" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" "sdhci_pci"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-amd"];
+  boot.extraModulePackages = [];
+  boot.kernelPackages = pkgs.linuxPackages;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
